5be3105f485859d0b6d01c63a9d09e835cb6313e
Giovane <91229782+DinizBH@users.noreply.github.com>
2025-01-04 19:12:06 -0300

    First Commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Main template file.
+ * 
+ * @package Vaniz
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wordpress theme</title>
+</head>
+<body>
+    <h1>Olá mundo SEAFujaseihjwsadefoisef</h1>
+</body>
+</html>
