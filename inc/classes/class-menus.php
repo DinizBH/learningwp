17ed31be71e32bdaafd729d5dae8336f7393571d
Giovane <91229782+DinizBH@users.noreply.github.com>
2025-01-16 23:20:38 -0300

    Add header menu structure

--- inc/classes/class-menus.php
+++ inc/classes/class-menus.php
@@ -19,7 +19,7 @@ class Menus{
         /**
          * Actions.
          */
-        add_action('init    ', [$this, 'register_menus']);
+        add_action('init', [$this, 'register_menus']);
     }
 
     public function register_menus(){
@@ -27,5 +27,29 @@ class Menus{
             'vaniz-header-menu' =>esc_html__('Header Menu', 'vaniz'),
             'vaniz-footer-menu' =>esc_html__('Footer Menu', 'vaniz'),
         ] );
+    }
+
+    public function get_menu_id($location){
+
+        //Get all locations.
+        $locations = get_nav_menu_locations();
+
+        //Get object id by location.
+        $menu_id = ! empty($locations[$location]) ? $locations[$location] : '';
+
+        return ! empty( $menu_id ) ? $menu_id : '';
+    }
+
+    public function get_child_menu_items($menu_array,$parent_id){
+        $child_menus = [];
+
+        if (!empty($menu_array) && is_array($menu_array)) {
+            foreach ($menu_array as $menu) {
+                if (intval($menu->menu_item_parent) === $parent_id) {
+                    array_push($child_menus,$menu);
+                }
+            }
+        }
+        return $child_menus;
     }
 }
